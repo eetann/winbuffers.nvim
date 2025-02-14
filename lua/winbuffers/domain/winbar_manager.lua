a9f7c7940eee027452b734b3285f8b97bf71b698
eetann <hideharu092@gmail.com>
2025-02-14 18:59:27 +0900

    feat: WIP improve unique buffer name handling
    Enhances the unique buffer name management by storing buffer numbers
    in a table, allowing for better handling of files with the same name.
    This change aims to prevent conflicts and ensure accurate buffer
    identification.

--- lua/winbuffers/domain/winbar_manager.lua
+++ lua/winbuffers/domain/winbar_manager.lua
@@ -2,12 +2,30 @@ local Winbar = require("winbuffers.domain.winbar")
 
 ---@class Winbuffers.WinbarManager
 ---@field winbar_table { [integer]: Winbuffers.Winbar }
----@field unique_buffer_names { [string]: integer } key is filename, value is bufnr
+---@field unique_buffer_names { [string]: { [integer]: boolean } } key is filename, value is bufnr
 local WinbarManager = {}
 WinbarManager.__index = WinbarManager
 
 function WinbarManager:new()
-	return setmetatable({ winbar_table = {} }, WinbarManager)
+	return setmetatable({ winbar_table = {}, unique_buffer_names = {} }, WinbarManager)
+end
+
+---@param bufnr integer
+function WinbarManager:get_unique_name(bufnr)
+	local bufinfo = vim.fn.getbufinfo(bufnr)[1]
+	local filename = vim.fn.fnamemodify(bufinfo.name, ":t")
+	local bufnr_list = self.unique_buffer_names[filename]
+	if bufnr_list == nil then
+		self.unique_buffer_names[filename] = {}
+		self.unique_buffer_names[filename][bufnr] = true
+		return filename
+	end
+	if #bufnr_list[filename] == 1 then
+		return filename
+	end
+	self.unique_buffer_names[filename][bufnr] = true
+	-- TODO: ユニークにして返す
+	return filename
 end
 
 ---attach buffer
@@ -20,6 +38,7 @@ function WinbarManager:attach_buffer(bufnr)
 		self.winbar_table[winid] = winbar
 	end
 	winbar:add_buffer(bufnr)
+
 	self:update(winbar)
 end
 
@@ -28,8 +47,7 @@ end
 function WinbarManager:create_text(winbar)
 	local text = ""
 	for _, key in ipairs(winbar:get_sorted_keys()) do
-		local bufinfo = vim.fn.getbufinfo(winbar.buffers[key].bufnr)[1]
-		text = text .. vim.fn.fnamemodify(bufinfo.name, ":t") .. " | "
+		text = text .. self:get_unique_name(winbar.buffers[key].bufnr) .. " | "
 	end
 	return text
 end
