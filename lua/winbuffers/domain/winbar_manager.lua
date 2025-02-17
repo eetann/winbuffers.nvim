fbbed82bcbbcb4308ea09fcd89de018700a7e058
eetann <hideharu092@gmail.com>
2025-02-18 08:45:24 +0900

    fix: winbar manager for unique name handling

--- lua/winbuffers/domain/winbar_manager.lua
+++ lua/winbuffers/domain/winbar_manager.lua
@@ -1,4 +1,3 @@
-local utils = require("winbuffers.domain.utils")
 local Winbar = require("winbuffers.domain.winbar")
 local UniqueNameManager = require("winbuffers.domain.unique_name_manager")
 
@@ -9,19 +8,7 @@ local WinbarManager = {}
 WinbarManager.__index = WinbarManager
 
 function WinbarManager:new()
-	return setmetatable({ winbar_table = {}, unique_name_manager = {} }, WinbarManager)
-end
-
----@param bufnr integer
----@return string
-function WinbarManager:get_unique_name(bufnr)
-	local bufinfo = vim.fn.getbufinfo(bufnr)[1]
-	local filename = vim.fn.fnamemodify(bufinfo.name, ":t")
-	local bufnr_list = self.unique_name_manager[filename]
-	if bufnr_list[bufinfo.bufnr] == nil then
-		return ""
-	end
-	return bufnr_list[bufinfo.bufnr].buffer_name
+	return setmetatable({ winbar_table = {}, unique_name_manager = UniqueNameManager:new() }, WinbarManager)
 end
 
 ---create text to display winbar
@@ -30,7 +17,7 @@ function WinbarManager:create_text(winbar)
 	local text = ""
 	local sorted_bufnrs = winbar:get_sorted_bufnrs()
 	for _, key in ipairs(sorted_bufnrs) do
-		local result = self:get_unique_name(winbar.buffers[key].bufnr)
+		local result = self.unique_name_manager:get_unique_name(winbar.buffers[key].bufnr)
 		text = text .. result .. " | "
 	end
 	return text
@@ -40,70 +27,6 @@ function WinbarManager:update()
 	for _, winbar in pairs(self.winbar_table) do
 		local text = self:create_text(winbar)
 		winbar:set_winbar(text)
-	end
-end
-
----get path with depth specified
----@param path_segment path_segment
----@param depth integer
----@return string
-function WinbarManager:get_depth_path(path_segment, depth)
-	local length = #path_segment
-	local start = length - depth -- 0 based index because vim function
-	if start < 0 then
-		start = 0
-	end
-	return table.concat(vim.fn.slice(path_segment, start, length), utils.sep)
-end
-
---- make paths unique
----@param buffer_name_maps { [bufnr]: buffer_name_map }
-function WinbarManager:make_path_unique(buffer_name_maps)
-	if vim.fn.len(vim.fn.filter(buffer_name_maps, "v:val isnot v:null")) == 1 then
-		for _, buffer_name_map in pairs(buffer_name_maps) do
-			buffer_name_map.buffer_name = buffer_name_map.path_segment[#buffer_name_map.path_segment]
-		end
-		return
-	end
-	local is_all_same_segument = false
-	local depth = 1
-	while not is_all_same_segument do
-		is_all_same_segument = true
-		depth = depth + 1
-		---@type true|string|nil
-		local first_segment = true
-		for _, buffer_name_map in pairs(buffer_name_maps) do
-			local start = #buffer_name_map.path_segment - (depth - 1)
-			local segment = buffer_name_map.path_segment[start]
-			if first_segment == true then
-				first_segment = segment
-			elseif first_segment ~= segment then
-				is_all_same_segument = false
-				break
-			end
-		end
-	end
-	depth = depth - 1
-	for bufnr, buffer_name_map in pairs(buffer_name_maps) do
-		buffer_name_maps[bufnr]["buffer_name"] = self:get_depth_path(buffer_name_map.path_segment, depth)
-	end
-end
-
----@param bufnr integer
----@param fullpath string
-function WinbarManager:add_to_unique_list(bufnr, fullpath)
-	local filename = vim.fn.fnamemodify(fullpath, ":t")
-	local buffer_name_maps = self.unique_buffer_names[filename]
-	if buffer_name_maps == nil then
-		self.unique_buffer_names[filename] = {
-			[bufnr] = {
-				path_segment = vim.split(fullpath, utils.sep),
-				buffer_name = filename,
-			},
-		}
-	else
-		self.unique_buffer_names[filename][bufnr] = { path_segment = vim.split(fullpath, utils.sep), buffer_name = "" }
-		self:make_path_unique(self.unique_buffer_names[filename])
 	end
 end
 
@@ -118,21 +41,9 @@ function WinbarManager:attach_buffer(bufnr)
 	end
 	winbar:add_buffer(bufnr)
 	local bufinfo = vim.fn.getbufinfo(bufnr)[1]
-	self:add_to_unique_list(bufnr, bufinfo.name)
+	self.unique_name_manager:add_to_unique_list(bufnr, bufinfo.name)
 
 	self:update()
-end
-
----@param bufnr integer
----@param fullpath string
-function WinbarManager:delete_from_unique_list(bufnr, fullpath)
-	local filename = vim.fn.fnamemodify(fullpath, ":t")
-	local buffer_name_maps = self.unique_buffer_names[filename]
-	if buffer_name_maps == nil then
-		return
-	end
-	self.unique_buffer_names[filename][bufnr] = nil
-	self:make_path_unique(self.unique_buffer_names[filename])
 end
 
 ---detach buffer
@@ -150,7 +61,7 @@ function WinbarManager:detach_buffer(bufnr)
 	end
 
 	local bufinfo = vim.fn.getbufinfo(bufnr)[1]
-	self:delete_from_unique_list(bufnr, bufinfo.name)
+	self.unique_name_manager:delete_from_unique_list(bufnr, bufinfo.name)
 
 	self:update()
 end
--- tests/test_winbar_manager.lua
+++ tests/test_winbar_manager.lua
@@ -15,51 +15,51 @@ local T = MiniTest.new_set({
 
 T["add_to_unique_list"] = function()
 	local manager = WinbarManager:new()
-	manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
-	manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
-	manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
-	eq(manager.unique_name_manager["buz.txt"], {
+	manager.unique_name_manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
+	manager.unique_name_manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
+	manager.unique_name_manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["buz.txt"], {
 		[1] = {
 			path_segment = { "", "home", "adam", "foo", "bar", "buz.txt" },
-			buffer_name = "buz.txt",
+			display_name = "buz.txt",
 		},
 	})
-	eq(manager.unique_name_manager["piyo.txt"], {
+	eq(manager.unique_name_manager.records_dict["piyo.txt"], {
 		[2] = {
 			path_segment = { "", "home", "adam", "foo", "bar", "piyo.txt" },
-			buffer_name = "bar/piyo.txt",
+			display_name = "bar/piyo.txt",
 		},
 		[3] = {
 			path_segment = { "", "home", "adam", "foo", "bar2", "piyo.txt" },
-			buffer_name = "bar2/piyo.txt",
+			display_name = "bar2/piyo.txt",
 		},
 	})
 end
 
 T["get_unique_name"] = function()
 	local manager = WinbarManager:new()
-	manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
-	manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
-	manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
-	eq(manager.unique_name_manager["buz.txt"][1].buffer_name, "buz.txt")
-	eq(manager.unique_name_manager["piyo.txt"][2].buffer_name, "bar/piyo.txt")
-	eq(manager.unique_name_manager["piyo.txt"][3].buffer_name, "bar2/piyo.txt")
+	manager.unique_name_manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
+	manager.unique_name_manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
+	manager.unique_name_manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["buz.txt"][1].display_name, "buz.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][2].display_name, "bar/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][3].display_name, "bar2/piyo.txt")
 end
 
 T["delete_from_unique_list"] = function()
 	local manager = WinbarManager:new()
-	manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
-	manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
-	eq(manager.unique_name_manager["piyo.txt"][2].buffer_name, "piyo.txt")
+	manager.unique_name_manager:add_to_unique_list(1, "/home/adam/foo/bar/buz.txt")
+	manager.unique_name_manager:add_to_unique_list(2, "/home/adam/foo/bar/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][2].display_name, "piyo.txt")
 
-	manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
-	eq(manager.unique_name_manager["piyo.txt"][2].buffer_name, "bar/piyo.txt")
+	manager.unique_name_manager:add_to_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][2].display_name, "bar/piyo.txt")
 
-	manager:delete_from_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
-	eq(manager.unique_name_manager["buz.txt"][1].buffer_name, "buz.txt")
-	eq(manager.unique_name_manager["piyo.txt"][2].buffer_name, "piyo.txt")
-	eq(manager.unique_name_manager["piyo.txt"][3], nil)
-	eq(vim.fn.len(vim.fn.filter(manager.unique_name_manager["piyo.txt"], "v:val isnot v:null")), 1)
+	manager.unique_name_manager:delete_from_unique_list(3, "/home/adam/foo/bar2/piyo.txt")
+	eq(manager.unique_name_manager.records_dict["buz.txt"][1].display_name, "buz.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][2].display_name, "piyo.txt")
+	eq(manager.unique_name_manager.records_dict["piyo.txt"][3], nil)
+	eq(vim.fn.len(vim.fn.filter(manager.unique_name_manager.records_dict["piyo.txt"], "v:val isnot v:null")), 1)
 end
 
 return T
