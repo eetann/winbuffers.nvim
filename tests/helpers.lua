5b9030ac38a106126e9d4d2bfe03e2e0c2aa2232
eetann <hideharu092@gmail.com>
2025-02-24 12:10:57 +0900

    refactor: improve winbar expectation handling
    Refactor winbar expectation functions to handle both strings and
    tables. This allows for more flexible test cases and improves
    readability by using a helper function to concatenate table values.

--- tests/helpers.lua
+++ tests/helpers.lua
@@ -3,33 +3,60 @@ local Helpers = {}
 Helpers.expect = vim.deepcopy(MiniTest.expect)
 
 Helpers.get_winbar = function(child, winid)
-	-- Because it cannot be get correctly by `child.wo[winid].winbar`
-	return child.api.nvim_get_option_value("winbar", { win = winid })
+  -- Because it cannot be get correctly by `child.wo[winid].winbar`
+  return child.api.nvim_get_option_value("winbar", { win = winid })
 end
 
-Helpers.expect.winbar_current_matching = MiniTest.new_expectation("current winbar matching", function(child, expected)
-	if type(child) == "string" then
-		return false
-	end
-	return child.wo.winbar == expected
-end, function(child, expected)
-	if type(child) == "string" then
-		return "Specify child as the first argument"
-	end
-	return string.format("received '%s' is not equal expected '%s'", child.wo.winbar, expected)
-end)
+---If it is a string, return the string as is.
+---If it is a table, concat and return string.
+---@param expected_value string|string[]
+---@return string
+local function toStringOrConcat(expected_value)
+  if type(expected_value) == "string" then
+    return expected_value
+  end
+  return table.concat(expected_value, "")
+end
 
-Helpers.expect.winbar_matching = MiniTest.new_expectation("winbar matching", function(child, winid, expected)
-	if type(child) == "string" or type(child) == "number" then
-		return false
-	end
-	return Helpers.get_winbar(child, winid) == expected
-end, function(child, winid, expected)
-	if type(child) == "string" or type(child) == "number" then
-		return "Specify child as the first argument"
-	end
-	return string.format("received '%s' is not equal expected '%s'", Helpers.get_winbar(child, winid), expected)
-end)
+Helpers.expect.winbar_current_matching = MiniTest.new_expectation(
+  "current winbar matching",
+  function(child, expected)
+    if type(child) == "string" then
+      return false
+    end
+    return child.wo.winbar == toStringOrConcat(expected)
+  end,
+  function(child, expected)
+    if type(child) == "string" then
+      return "Specify child as the first argument"
+    end
+    return string.format(
+      "received '%s' is not equal expected '%s'",
+      child.wo.winbar,
+      toStringOrConcat(expected)
+    )
+  end
+)
+
+Helpers.expect.winbar_matching = MiniTest.new_expectation(
+  "winbar matching",
+  function(child, winid, expected)
+    if type(child) == "string" or type(child) == "number" then
+      return false
+    end
+    return Helpers.get_winbar(child, winid) == toStringOrConcat(expected)
+  end,
+  function(child, winid, expected)
+    if type(child) == "string" or type(child) == "number" then
+      return "Specify child as the first argument"
+    end
+    return string.format(
+      "received '%s' is not equal expected '%s'",
+      Helpers.get_winbar(child, winid),
+      toStringOrConcat(expected)
+    )
+  end
+)
 
 -- ---@type fun(received: any, expected: any)
 -- Helpers.expect.toContain = MiniTest.new_expectation("contain value in array", function(received, expected)
@@ -44,20 +71,20 @@ end)
 -- end)
 
 Helpers.new_child_neovim = function()
-	local child = MiniTest.new_child_neovim()
+  local child = MiniTest.new_child_neovim()
 
-	---@diagnostic disable-next-line: inject-field
-	child.setup = function()
-		child.restart({ "-u", "scripts/test/minimal_init.lua" })
-		child.bo.readonly = false
-	end
+  ---@diagnostic disable-next-line: inject-field
+  child.setup = function()
+    child.restart({ "-u", "scripts/test/minimal_init.lua" })
+    child.bo.readonly = false
+  end
 
-	---@diagnostic disable-next-line: inject-field
-	child.load = function(config)
-		child.lua("require('winbuffers').setup(...)", { config })
-	end
+  ---@diagnostic disable-next-line: inject-field
+  child.load = function(config)
+    child.lua("require('winbuffers').setup(...)", { config })
+  end
 
-	return child
+  return child
 end
 
 return Helpers
--- tests/test_simple.lua
+++ tests/test_simple.lua
@@ -3,61 +3,90 @@ local child = Helpers.new_child_neovim()
 local expect, eq = Helpers.expect, Helpers.expect.equality
 
 local T = MiniTest.new_set({
-	hooks = {
-		pre_case = function()
-			child.setup()
-			child.load()
-		end,
-		post_once = child.stop,
-	},
+  hooks = {
+    pre_case = function()
+      child.setup()
+      child.load()
+    end,
+    post_once = child.stop,
+  },
 })
 
 T["works with split command"] = function()
-	-- | 1 |
-	local win1 = child.api.nvim_get_current_win()
-	child.cmd("edit src/foo.lua")
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#foo.lua%#Normal# ")
+  -- | 1 |
+  local win1 = child.api.nvim_get_current_win()
+  child.cmd("edit src/foo.lua")
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "foo.lua", "%#Normal# " }
+  )
 
-	-- | 2 | 1 |
-	-- 2
-	child.cmd("vsplit src/bar.lua")
-	local win2 = child.api.nvim_get_current_win()
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#bar.lua%#Normal# ")
-	expect.winbar_matching(child, win1, "%#WinBuffersFocusCurrentBufferTab#foo.lua%#Normal# ")
+  -- | 2 | 1 |
+  -- 2
+  child.cmd("vsplit src/bar.lua")
+  local win2 = child.api.nvim_get_current_win()
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "bar.lua", "%#Normal# " }
+  )
+  expect.winbar_matching(
+    child,
+    win1,
+    { "%#WinBuffersFocusCurrentBufferTab#", "foo.lua", "%#Normal# " }
+  )
 
-	-- | 3 |   |
-	-- |---| 1 |
-	-- | 2 |   |
-	-- 3
-	child.cmd("split foo/buz/buz.lua")
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#buz.lua%#Normal# ")
-	expect.winbar_matching(child, win2, "%#WinBuffersFocusCurrentBufferTab#bar.lua%#Normal# ")
+  -- | 3 |   |
+  -- |---| 1 |
+  -- | 2 |   |
+  -- 3
+  child.cmd("split foo/buz/buz.lua")
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "buz.lua", "%#Normal# " }
+  )
+  expect.winbar_matching(
+    child,
+    win2,
+    { "%#WinBuffersFocusCurrentBufferTab#", "bar.lua", "%#Normal# " }
+  )
 end
 
 T["works without split command"] = function()
-	-- | 1 |
-	child.cmd("edit src/foo/bar.lua")
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#bar.lua%#Normal# ")
+  -- | 1 |
+  child.cmd("edit src/foo/bar.lua")
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab", "#bar.lua", "%#Normal# " }
+  )
 
-	-- | 2 | 1 |
-	-- 2
-	child.cmd("vsplit")
-	child.cmd("edit src/foo2/bar.lua")
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#foo2/bar.lua%#Normal# ")
-	-- 1
-	child.cmd("wincmd l") -- jump right
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#foo/bar.lua%#Normal# ")
+  -- | 2 | 1 |
+  -- 2
+  child.cmd("vsplit")
+  child.cmd("edit src/foo2/bar.lua")
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "foo2/bar.lua", "%#Normal# " }
+  )
+  -- 1
+  child.cmd("wincmd l") -- jump right
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "foo/bar.lua", "%#Normal# " }
+  )
 end
 
 T["delete buffer"] = function()
-	-- | 1 |
-	child.cmd("edit src/foo/bar.lua")
+  -- | 1 |
+  child.cmd("edit src/foo/bar.lua")
 
-	-- | 2 | 1 |
-	-- 2
-	child.cmd("vsplit src/foo2/bar.lua")
-	child.cmd("bd")
-	expect.winbar_current_matching(child, "%#WinBuffersFocusWindowTab#bar.lua%#Normal# ")
+  -- | 2 | 1 |
+  -- 2
+  child.cmd("vsplit src/foo2/bar.lua")
+  child.cmd("bd")
+  expect.winbar_current_matching(
+    child,
+    { "%#WinBuffersFocusWindowTab#", "bar.lua", "%#Normal# " }
+  )
 end
 
 return T
